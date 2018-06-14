98c215b0e1281a9dff2ed1bee53618478fcbf8cc
EvgenBES <40202338+EvgenBES@users.noreply.github.com>
2018-06-14 10:24:48 +0300

    Main activity - updated. Made buttonDz1 - active click and transition to 2Activity

--- app/src/main/java/com/example/fox/besproject/MainActivity.java
+++ app/src/main/java/com/example/fox/besproject/MainActivity.java
@@ -1,13 +1,34 @@
 package com.example.fox.besproject;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button buttonDz1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        buttonDz1 = (Button) findViewById(R.id.buttonDz1);
+        buttonDz1.setOnClickListener(this);
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.buttonDz1:
+                Intent intent = new Intent(this, Dz1Activity.class);
+                startActivity(intent);
+                break;
+            default:
+                break;
+        }
     }
 }
